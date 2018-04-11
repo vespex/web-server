ad7bea37af8694a837bc7cc2f691c34c226cbb2d
gaoxin <gaoxin@maijia.com>
2018-04-11 16:36:10 +0800

    update example

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ejs = require('ejs')
+// var cors = require('cors')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -18,6 +19,7 @@ app.engine('html', ejs.__express);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
