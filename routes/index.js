8f5df13f5f4ca478f66669936fb65a8e31937d2a
gaoxin <gaoxin@maijia.com>
2018-04-28 14:10:36 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 var multer = require('multer')
 var isDev = process.env.NODE_ENV === 'development'
 // require('../data/db.js')
+/* upload */
 const storage = multer.diskStorage({
   destination (req, file, cb) {
     cb(null, 'public/api')
@@ -16,6 +17,7 @@ const storage = multer.diskStorage({
 
 const api = multer({ storage })
 
+/* router list */
 const route = [ // path配置 如无其他配置 需以/结尾
   { name: 'root', path: '/', data: { title: 'xinge\'s page'} },
   { name: 'index', path: '/index/' },
@@ -35,6 +37,7 @@ if (isDev) {
   var server = require('https').createServer(options, express());
 }
 
+/* socket.io */
 var io = require('socket.io')(server);
 
 server.listen(3030);
@@ -54,7 +57,7 @@ io.on('connection', (socket) => {
 
 });
 
-/* GET home page. */
+/* GET page */
 route.forEach(item => {
   router.get(item.path, (req, res, next) => {
     let reqPath = req.path.endsWith('/') ? (req.path + 'index') : req.path
@@ -62,128 +65,12 @@ route.forEach(item => {
   });
 })
 
+/* POST */
 router.post('/swagger/submit', api.single('doc'), (req, res, next) => {
   if(req.file) {
     return res.json({ result: 1, message: '上传成功' })
   }
   return res.json({ result: 0, message: '上传失败' })
-});
-
-const list = [
-  {
-    id: 1,
-    avatar: 'https://dummyimage.com/160x160',
-    title: '运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装',
-    spec: 'S码 蓝色',
-    price: '198.00',
-    num: 2,
-    min: 2,
-    max: 10,
-  },
-  {
-    id: 2,
-    avatar: 'https://dummyimage.com/160x160',
-    title: '运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装',
-    spec: 'S码 蓝色',
-    price: '198.00',
-    num: 10,
-    min: 1,
-    max: 200,
-  },
-  {
-    id: 3,
-    avatar: 'https://dummyimage.com/160x160',
-    title: '运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装',
-    spec: 'S码 蓝色',
-    price: '198.00',
-    num: 6,
-    min: 5,
-    max: 200,
-  },
-  {
-    id: 4,
-    avatar: 'https://dummyimage.com/160x160',
-    title: '运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装',
-    spec: 'S码 蓝色',
-    price: '198.00',
-    num: 2,
-    min: 2,
-    max: 10,
-  },
-  {
-    id: 5,
-    avatar: 'https://dummyimage.com/160x160',
-    title: '运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装',
-    spec: 'S码 蓝色',
-    price: '198.00',
-    num: 10,
-    min: 1,
-    max: 200,
-  },
-  {
-    id: 6,
-    avatar: 'https://dummyimage.com/160x160',
-    title: '运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装',
-    spec: 'S码 蓝色',
-    price: '198.00',
-    num: 6,
-    min: 5,
-    max: 200,
-  },
-  {
-    id: 7,
-    avatar: 'https://dummyimage.com/160x160',
-    title: '运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装',
-    spec: 'S码 蓝色',
-    price: '198.00',
-    num: 2,
-    min: 2,
-    max: 10,
-  },
-  {
-    id: 8,
-    avatar: 'https://dummyimage.com/160x160',
-    title: '运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装',
-    spec: 'S码 蓝色',
-    price: '198.00',
-    num: 10,
-    min: 1,
-    max: 200,
-  },
-  {
-    id: 9,
-    avatar: 'https://dummyimage.com/160x160',
-    title: '运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装 运动服套装女2018春季新款韩版春装',
-    spec: 'S码 蓝色',
-    price: '198.00',
-    num: 6,
-    min: 5,
-    max: 200,
-  }
-]
-
-router.post('/mp/cart/update', (req, res, next) => {
-  const { changeList } = req.body
-  changeList.forEach(item => {
-    let {id, ...data} = item
-    let {num} = data
-    if (num === 0) {
-      list.splice(list.findIndex(itm => itm.id === id), 1)
-    } else {
-      let cartItem = list.find(itm => itm.id === id)
-      cartItem && Object.assign(cartItem, data)
-    }
-  })
-  res.send({ status: 1, data: { list }, message: 'ok' })
-});
-
-router.get('/mp/cart/list', (req, res, next) => {
-  res.send({ status: 1, data: { list }, message: 'ok' })
-});
-router.get('/api/test', (req, res, next) => {
-  setTimeout(() => {
-    res.send({ status: 1, data: { test: 'test' }, message: 'ok' })
-  }, 1000)
 });
 
 module.exports = router;
